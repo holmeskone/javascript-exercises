2f6a06608aebf756bbcab57ee9593ccda59a7a02
holmeskone <[165698602+holmeskone@users.noreply.github.com]>
2024-08-22 20:33:30 +0100

    Add loop to return reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    result = '';
+    let characters = str.split("");
+    let reverseCharacters = characters.reverse().join('');
+    return(reverseCharacters);
 };
 
 // Do not edit below this line

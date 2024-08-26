4a92d2932819cd4fa187f1748dd24e2071b540c3
holmeskone <[165698602+holmeskone@users.noreply.github.com]>
2024-08-26 13:42:58 +0200

    Create if statement then loop thorugh incremental values and add to array

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(numStart,numEnd) {
+    numbers = [];
+    // Ensure start is less than or equal to end
+  if (start > end) {
+    [numStart, numEnd] = [numEnd, numStart];
+  }
+  for (let i = numStart; i <= numEnd; i++) {
+    numbers.push(i);
+  }
+  
+  return numbers;
 };
 
 // Do not edit below this line

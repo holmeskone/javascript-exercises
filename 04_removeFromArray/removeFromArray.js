77847f2fd7e89e07107acbaca34a90a188c18cb2
holmeskone <[165698602+holmeskone@users.noreply.github.com]>
2024-08-26 13:39:30 +0200

    All passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,11 @@
-const removeFromArray = function(arr,num) {
-    arr = arr.filter(value => value !== num);
+const removeFromArray = function(arr, ...nums) {
+    // Loop through each number in nums
+    nums.forEach(num => {
+        // Filter the array to remove the current number
+        arr = arr.filter(value => value !== num);
+    });
     return arr;
-};
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;

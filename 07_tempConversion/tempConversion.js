936fed8defa361b7199c88d2c9649d68931c9501
holmeskone <[165698602+holmeskone@users.noreply.github.com]>
2024-09-04 17:38:27 +0100

    Practice

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celcius = (fahrenheit - 32) * 5/9;
+  return Math.round(celcius*10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  const fahr = (cels * 9/5) + 32 ;
+  return Math.round(fahr*10) / 10
 };
 
 // Do not edit below this line

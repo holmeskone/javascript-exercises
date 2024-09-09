8bb208b5c2380b29704865120a78308eb0bf2cf5
holmeskone <[165698602+holmeskone@users.noreply.github.com]>
2024-09-09 16:06:05 +0100

    join all characters without spaces or punctuation

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,50 @@
-const palindromes = function () {
+const palindromes = (text) => {
+    const words = text // Split each text into words
+        .toLowerCase()
+        .split(/[ ,\.]+/);
+
+    const letters = words
+        .join('');
+
+    console.log((words));
+
+    for (let i=0; i<letters.length; i++){
+
+    }
+
+
 
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+/*
+Check - Get the text
+
+Check - remove all spaces and non-letter characters.
+
+put it into an array or some sort of place
+
+and check if the first character and the last are the same
+then add 1 to the first and subtract 1 to the last, loop through that process 
+and stop when the character[i] is the same. 
+
+return true if the same
+false if not. 
+
+
+
+-----
+
+Get the text
+remove space and punctuation 
+put into array
+loop through each value
+create a reversed array 
+loop through each value
+
+if values are the same its a palindrom
+
+*/
